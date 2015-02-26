c2b402663c3fcfadcee1f6860973743feb10d0f0
Alan Mosca <alanm@waniasset.com>
2015-02-26 16:33:23 +0000

    fixed

--- cachematrix.R
+++ cachematrix.R
@@ -6,18 +6,17 @@
 ## once it is calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
-        list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+    m <- NULL
+    set <- function(y) {
+            x <<- y
+            m <<- NULL
     }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
